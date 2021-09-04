587f1c8d675035c81f0e61e0977744e6bc53f8d6
poonam2096740 <85899531+poonam2096740@users.noreply.github.com>
2021-09-04 19:03:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -49,11 +49,11 @@ var last_position_of_x, last_position_of_y;
         ctx.moveTo(last_position_of_x, last_position_of_y);
 
         console.log("Current position of x and y coordinates = ");
-        console.log("x  = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
-        ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
+        console.log("x  = " + current_position_of_touch_x + "y = " + current_position_of_touch_y);
+        ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
         ctx.stroke();
-        last_position_of_x = current_position_of_mouse_x; 
-        last_position_of_y = current_position_of_mouse_y;
+        last_position_of_x = current_position_of_touch_x; 
+        last_position_of_y = current_position_of_touch_y;
     }
     
     function clear_area()
